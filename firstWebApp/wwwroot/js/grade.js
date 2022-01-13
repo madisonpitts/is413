e5854c8030d30e6cda720ec617122b0c9e7bb45a
madisonpitts <90300881+madisonpitts@users.noreply.github.com>
2022-01-12 19:02:28 -0700

    adding comments

--- firstWebApp/wwwroot/js/grade.js
+++ firstWebApp/wwwroot/js/grade.js
@@ -1,20 +1,21 @@
 ﻿$("#calcbutton").click(function () {
-  
+  //retrieving values from inputs and assigning them values
     var assignment = $("#assignment").val();
     var groupproject = $("#groupproject").val();
     var quizzes = $("#quizzes").val();
     var exams = $("#exams").val();
     var intex = $("#intex").val();
-
+    //calculating the grade weight based off of the class weight from the syllabus
     var assignmentweight = .55 * assignment;
     var projectweight = .05 * groupproject;
     var quizweight = .10 * quizzes;
     var examweight = .20 * exams;
     var intexweight = .10 * intex;
+    //adding them all together to get the final grade
     var numericgrade = parseFloat(assignmentweight) + parseFloat(projectweight) + parseFloat(quizweight) + parseFloat(examweight) + parseFloat(intexweight);
 
 
-
+    //if statement to figure out the letter grade
     if (numericgrade >= 94) {
         var lettergrade = "A";
     } else if (numericgrade >= 90) {
@@ -40,6 +41,6 @@
     } else {
         var lettergrade = "E";
     }
-
+    //alert to tell the user what the grade would be
     alert("You will receive a " + numericgrade + "% in the class which results in a " + lettergrade)
 })
