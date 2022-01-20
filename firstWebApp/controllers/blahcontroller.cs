a00b434c9bf2c15acc0688a84c16423a2428bdf9
madisonpitts <madpitts@byu.edu>
2022-01-19 17:43:56 -0700

    converting to mvc

---
+++ firstWebApp/controllers/blahcontroller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using firstWebApp.models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace firstWebApp.controllers
+{
+    public class Blahcontroller : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculator(calculatormodel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ firstWebApp/models/calculatormodel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace firstWebApp.models
+{
+    public class calculatormodel
+    {
+        [Range(0,100)]
+        public int assignment { get; set; }
+
+        [Range(0, 100)]
+        public int groupproject { get; set; }
+
+        [Range(0, 100)]
+        public int quizzes { get; set; }
+
+        [Range(0, 100)]
+        public int exams { get; set; }
+
+        [Range(0, 100)]
+        public int intex { get; set; }
+
+      
+    }
+}
